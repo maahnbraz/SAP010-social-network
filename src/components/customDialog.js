c5e42e0324b7fec074c421c0c205ea725a028d48
maahnbraz <marinanbraz@gmail.com>
2023-07-05 18:36:12 -0300

    arrumando os erros de identação

--- src/components/customDialog.js
+++ src/components/customDialog.js
@@ -1,8 +1,8 @@
 export default (text, onClickOk) => {
-  const dialog = document.createElement('div')
-  dialog.classList.add('dialog-background')
+  const dialog = document.createElement('div');
+  dialog.classList.add('dialog-background');
 
-  const body = document.querySelector('html')
+  const body = document.querySelector('html');
 
   const content = `
     <div class='dialog'>
@@ -12,22 +12,22 @@ export default (text, onClickOk) => {
         <button id='btn-ok'>OK</button>
       </div>
     </div>
-  `
-  dialog.innerHTML = content
+  `;
+  dialog.innerHTML = content;
 
   const inputMessage = dialog.querySelector('#input-message');
   const btnCancel = dialog.querySelector('#btn-cancel');
   const btnOk = dialog.querySelector('#btn-ok');
-  
+
   btnCancel.addEventListener('click', () => {
-    body.removeChild(dialog)
-  })
+    body.removeChild(dialog);
+  });
 
   btnOk.addEventListener('click', () => {
-    const newText = inputMessage.value
-    onClickOk(newText)
-    body.removeChild(dialog)
-  })
+    const newText = inputMessage.value;
+    onClickOk(newText);
+    body.removeChild(dialog);
+  });
 
   body.appendChild(dialog);
-}
+};
--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -47,7 +47,7 @@ export default () => {
       new Date(),
       auth.currentUser.displayName,
       inputText.value,
-      auth.currentUser.uid
+      auth.currentUser.uid,
     )
       .then(() => {
         customAlert('Seu post foi publicado com sucesso');
@@ -61,7 +61,7 @@ export default () => {
 
   const buttonLogOut = feedContainer.querySelector('#button-logout');
   buttonLogOut.addEventListener('click', () => {
-    customDialog('Deseja realmente sair?', () =>{
+    customDialog('Deseja realmente sair?', () => {
       logOut()
         .then(() => {
           window.location.hash = '#login';
@@ -83,7 +83,7 @@ async function showFeed() {
 
   feedElement.innerHTML = '';
 
-  posts.forEach(post => {
+  posts.forEach((post) => {
     const postElement = createPostElement(post, feedElement);
     feedElement.appendChild(postElement);
   });
@@ -103,18 +103,16 @@ function createPostElement(post, feedElement) {
   const data = new Date(dataEmMilissegundos);
 
   const ano = data.getFullYear();
-  const mes = ('0' + (data.getMonth() + 1)).slice(-2);
-  const dia = ('0' + data.getDate()).slice(-2);
-  const hora = ('0' + data.getHours()).slice(-2);
-  const minuto = ('0' + data.getMinutes()).slice(-2);
+  const mes = (`0${data.getMonth() + 1}`).slice(-2);
+  const dia = (`0${data.getDate()}`).slice(-2);
+  const hora = (`0${data.getHours()}`).slice(-2);
+  const minuto = (`0${data.getMinutes()}`).slice(-2);
 
-  const btnEdit =
-    post.uid === auth.currentUser.uid
-      ? '<p class="button-edit"><svg xmlns="http://www.w3.org/2000/svg" class="icons-post" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></p>' : '';
+  const btnEdit = post.uid === auth.currentUser.uid
+    ? '<p class="button-edit"><svg xmlns="http://www.w3.org/2000/svg" class="icons-post" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></p>' : '';
 
-  const btnDelete =
-    post.uid === auth.currentUser.uid
-      ? '<p class="button-delete"id="button-delete"><svg xmlns="http://www.w3.org/2000/svg" class="icons-post" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg></p>' : '';
+  const btnDelete = post.uid === auth.currentUser.uid
+    ? '<p class="button-delete"id="button-delete"><svg xmlns="http://www.w3.org/2000/svg" class="icons-post" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg></p>' : '';
 
   const content = `
     <div class="informations">
@@ -152,7 +150,7 @@ function createPostElement(post, feedElement) {
       // então vamos tirar ele do array de likes
       if (likesArray.includes(currentUser)) {
         await deslikeCounter(post.id, currentUser);
-        //foi removido do array de likes dessa publicação lá no Firebase
+        // foi removido do array de likes dessa publicação lá no Firebase
         // mas ainda precisamos tirar o usuario do array que esta na variavel local
         const index = likesArray.indexOf(currentUser);
         if (index !== -1) {
@@ -179,9 +177,7 @@ function createPostElement(post, feedElement) {
         await deletePost(post.id);
 
         feedElement.removeChild(postElement);
-      })
-
-
+      });
     });
   }
 
@@ -189,7 +185,7 @@ function createPostElement(post, feedElement) {
   const buttonEdit = postElement.querySelector('.button-edit');
   if (buttonEdit) {
     buttonEdit.addEventListener('click', () => {
-      //const newText = prompt('Digite o novo texto:');
+      // const newText = prompt('Digite o novo texto:');
       customEditDialog(post.text, (newText) => {
         const postId = postElement.getAttribute('data-post-id');
         if (newText) {
@@ -197,18 +193,18 @@ function createPostElement(post, feedElement) {
             .then(() => {
               const textElement = postElement.querySelector('.text');
               textElement.textContent = newText;
-              //atualiza a variável post localmente
+              // atualiza a variável post localmente
               post.text = newText;
               customAlert('Post atualizado com sucesso!');
             })
-            .catch(error => {
+            .catch((error) => {
               console.error('Erro ao editar o post:', error);
               customAlert(
-                'Ocorreu um erro ao editar o post. Por favor, tente novamente mais tarde.'
+                'Ocorreu um erro ao editar o post. Por favor, tente novamente mais tarde.',
               );
             });
         }
-      })
+      });
     });
   }
 
